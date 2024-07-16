b9773755f769986c799b623ec3e4edac8f6f1a30
sefa <59339079+sefaalanur@users.noreply.github.com>
2024-07-16 05:23:53 +0200

    1st stage

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Todos } from './components/Todos';
+function App() {
+  return (
+    <div>
+        <Todos items={['Learn React','Learn TypeScript']}/>
+    </div>
+  );
+}
+
+export default App;
